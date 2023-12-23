0c5b1aa8e74d26fd3bf2ebaada63f00f184e15df
Ryan Arzenti <106203065+rarzenti@users.noreply.github.com>
2023-12-23 14:57:02 -0500

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -44,8 +44,8 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
   
-          <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
-          <p className="text-[#ADB7BE] mb-4 max-w-md">I'm currently looking for new opportunities.
+          <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
+          <p className="text-[#ADB7BE] mb-4 max-w-md">I&apos;m currently looking for new opportunities.
           </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/rarzenti">
