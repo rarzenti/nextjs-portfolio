3b28adccea1e32fad4957418ca501c428656e046
Ryan Arzenti <106203065+rarzenti@users.noreply.github.com>
2023-12-23 14:57:25 -0500

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <motion.div initial={{ opacity: 0, scale:0.5 }} animate={{ opacity: 1, scale:1}} transition={{ duration:0.75}}className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
               <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal  font-extrabold">
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, I'm{" "}</span>
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, I&apos;m{" "}</span>
                 <br></br>
                 <TypeAnimation
                   sequence={[
